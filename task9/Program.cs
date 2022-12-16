e4b8104f4f15547b902c48a61ca91cbcab097434
Ocheretianova <lariekaterina375@gmail.com>
2022-12-16 17:23:58 +0300

    Add task9

---
+++ task9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+Console.Write("Введите первое число: ");
+Double a = Double.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+Double b = Double.Parse(Console.ReadLine()!);
+
+
+   if ( a % b == 0 )
+   {
+        Console.Write("Первое число кратно второму");
+   }
+   else
+   {
+    Console.Write("Первое число некратно второму");
+   }
