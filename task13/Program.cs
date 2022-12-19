ec8ca9e21dc733d0c5ff4ac8b05e7e3b4c278799
Ocheretianova <lariekaterina375@gmail.com>
2022-12-19 16:32:28 +0300

    add task13

---
+++ task13/Program.cs
@@ -0,0 +1,14 @@
+﻿// программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет
+int num = new Random().Next(1, 3000);
+Console.Write(num);
+int a = num / 10 % 10;
+Console.WriteLine();
+if (num / 10 > 0)
+{
+ Console.WriteLine(a);
+} 
+else 
+{
+   Console.WriteLine("У числа нет третьей цифры");
+}
