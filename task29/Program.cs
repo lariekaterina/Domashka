807a802fd79c2348ec9379ee547fe88951d25d13
Ocheretianova <lariekaterina375@gmail.com>
2022-12-20 23:51:33 +0300

    add task29

---
+++ task29/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая задаёт массив
+// из 8 случайных чисел (-10, 10) и выводит их на экран
+int[] Array = new int[8];
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] = new Random().Next(-10, 11);
+    Console.Write(" " + Array[i]);
+}
