af4e9b97cea1f300f0a39b541cf38416c12317b9
Ocheretianova <lariekaterina375@gmail.com>
2022-12-26 12:20:23 +0300

    add task41

---
+++ task41/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите числа через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество элементов больше 0: {count}");
